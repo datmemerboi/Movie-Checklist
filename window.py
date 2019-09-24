e8d42f3ecf28fbdb6f6f408adae92305726080ab
datmemerboi <shravanssd@gmail.com>
2019-09-24 19:30:22 +0530

    First Update

--- window.py
+++ window.py
@@ -2,4 +2,4 @@ class WindowClass(object):
 	def WindowFn(self,window):
 		window.setWindowTitle("Window Class")
 		window.resize(600,600)
-		window.setStyleSheet("background-color:rgb(0, 0, 0);")
+		# window.setStyleSheet("background-color:rgb(0, 0, 0);")
