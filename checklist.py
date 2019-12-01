e66e999d83c1a6ebef7dfcc671b7bc9ca9bb1a1b
datmemerboi <shravanssd@gmail.com>
2019-12-01 20:24:46 +0530

    Tenth Checkpoint

--- checklist.py
+++ checklist.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, QtCore
+from PyQt5 import QtWidgets
 from pymongo import MongoClient
 recommendBool = False
 
--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-"""
+'''
 This is the file to setup your system for Movie Checklist.
 Avoid it if you have the softwares pre-installed.
 
 Softwares Required: Python3, PyQt5, MongoDB, PyMongo
-"""
+'''
 
 # Installing PyQt5
 import os
+print("Installing PyQt5..")
 os.system("pip3 install --user pyqt5")
 os.system("sudo apt-get install python3-pyqt5")
-os.system("qtchooser -run-tool=designer -qt=5")
 
 # Installing PyMongo
+print("Installing PyMongo..")
 os.system("python3 -m pip install pymongo")
