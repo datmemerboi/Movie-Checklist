c98aaae8b91bea025ec5bc243cf4780313250e0e
datmemerboi <shravanssd@gmail.com>
2019-12-02 18:07:27 +0530

    Tenth Checkpoint

--- checklist.py
+++ checklist.py
@@ -1,5 +1,6 @@
 from PyQt5 import QtWidgets
 from pymongo import MongoClient
+import os, backupnow
 recommendBool = False
 
 class ChecklistClass(object):
@@ -9,18 +10,33 @@ class ChecklistClass(object):
 		File = open("css/input.css", 'r')
 		inputCSS = File.read().strip()
 		File.close()
-
 		File = open("css/button.css", 'r')
 		buttonCSS = File.read().strip()
 		File.close()
+		File = open("./css/menubar.css",'r')
+		menubarCSS = File.read().strip()
+		File.close()
+		File = open("./css/label.css", 'r')
+		labelCSS = File.read().strip()
+		File.close()
 
-		PrintBtn = QtWidgets.QPushButton(window)
-		PrintBtn.setStyleSheet(buttonCSS)
-		PrintBtn.move(500, 0)
-		PrintBtn.resize(70, 30)
-		PrintBtn.setText("Print")
-		PrintBtn.clicked.connect(lambda: self.PrintListFn())
+		menuBar = window.menuBar()
+		menuBar.setStyleSheet(menubarCSS)
 
+		backupMenu = menuBar.addMenu("Backup")
+		backupChecklistMenu = backupMenu.addAction("Backup Checklist")
+		backupChecklistMenu.triggered.connect(lambda: backupnow.BackupNowFn("Movie", "Checklist"))
+		backupRecommendedMenu = backupMenu.addAction("Backup Recommended")
+		backupRecommendedMenu.triggered.connect(lambda: backupnow.BackupNowFn("Movie", "Recommend"))
+
+		searchMenu = menuBar.addMenu("Search")
+		searchByDirector = searchMenu.addAction("By Director")
+		searchByDirector.triggered.connect(lambda: os.system("python3 printdirector.py"))
+		searchByYear = searchMenu.addAction("By Year")
+		searchByYear.triggered.connect(lambda: os.system("python3 printyear.py"))
+		searchByLang = searchMenu.addAction("By Language")
+		searchByLang.triggered.connect(lambda: os.system("python3 printlanguage.py"))
+		
 		self.title = QtWidgets.QLineEdit(window)
 		self.title.setStyleSheet(inputCSS)
 		self.title.resize(350, 45)
@@ -51,32 +67,37 @@ class ChecklistClass(object):
 		self.remarks.resize(350, 150)
 		self.remarks.move(125, 340)
 
-		AddBtn = QtWidgets.QPushButton(window)
-		AddBtn.setStyleSheet(buttonCSS)
-		AddBtn.move(230, 525)
-		AddBtn.resize(70, 30)
-		AddBtn.setText("Check")
-		AddBtn.clicked.connect(lambda: self.InsertCheckedFn())
+		addBtn = QtWidgets.QPushButton(window)
+		addBtn.setStyleSheet(buttonCSS)
+		addBtn.setText("Add")
+		addBtn.resize(70, 30)
+		addBtn.move(230, 525)
+		addBtn.clicked.connect(lambda: self.CheckExistingFn())
+		addBtn.clicked.connect(lambda: self.InsertCheckedFn())
 
 		recommendBtn = QtWidgets.QCheckBox(window)
 		recommendBtn.setChecked(False)
 		recommendBtn.setStyleSheet(buttonCSS)
-		recommendBtn.setText("Recommend")
-		recommendBtn.move(320, 525)
-		recommendBtn.resize(100, 30)
+		recommendBtn.resize(15, 30)
+		recommendBtn.move(320, 480)
 		recommendBtn.stateChanged.connect(lambda:self.RecommendFn())
+		recommendText = QtWidgets.QLabel(window)
+		recommendText.setText("Recommend?")
+		recommendText.setStyleSheet(labelCSS)
+		recommendText.move(340, 478)
 
-	def PrintListFn(self):
-		mongoconn = MongoClient("mongodb://localhost:27017")
-		print("\r")
-		for index in ( mongoconn.Movie.Checklist.find() ):
-			print("\033[1m"+index['Title'],"\033[0m | ",index['Director']," | ",index['Year']," | ",index['Language'])
+		printListBtn = QtWidgets.QPushButton(window)
+		printListBtn.setStyleSheet(buttonCSS)
+		printListBtn.resize(180, 30)
+		printListBtn.move(215, 540)
+		printListBtn.setText("Print Checklist")
+		printListBtn.clicked.connect(lambda: os.system("python3 printlist.py"))
 
 	def CheckExistingFn(self):
 		mongoconn = MongoClient("mongodb://localhost:27017")
 		query = mongoconn.Movie.Checklist.find({"Title":self.title.text().strip()})
 		for i in query:
-			if(i['Title'] == self.title.text().strip()):
+			if(i['Title'] == self.title.text().strip() and i["Director"] == self.director.text().strip()):
 				ExistingMessage = QtWidgets.QMessageBox()
 				ExistingMessage.setText("Already in List")
 				ExistingMessage.setWindowTitle("Done!")
@@ -85,14 +106,16 @@ class ChecklistClass(object):
 		mongoconn.close()
 
 	def InsertCheckedFn(self):
-		row = {"Title":self.title.text().strip(), "Director":self.director.text().strip(), "Year":self.year.text().strip(), "Language":self.language.currentText(), "Remarks":self.remarks.toPlainText().strip()};
 		if(not(self.CheckExistingFn())):
-			if(row['Title']==''):
+			if(self.title.text().strip()=='' or self.director.text().strip()=='' or self.year.text().strip()==''):
 				NullMessage = QtWidgets.QMessageBox()
-				NullMessage.setText("No values to enter")
+				NullMessage.setText("Missing few values")
 				NullMessage.setWindowTitle("Error!")
 				NullMessage.exec_()
 			else:
+				row = {"Title":self.title.text().strip(), "Director":self.director.text().strip(), "Year":self.year.text().strip(), "Language":self.language.currentText()};
+				if(self.remarks.toPlainText().strip()!=''):
+					row["Remarks"] = self.remarks.toPlainText().strip()
 				mongoconn = MongoClient("mongodb://localhost:27017")
 				query = mongoconn.Movie.Checklist.update(row, row, upsert = True)
 				CheckedMessage = QtWidgets.QMessageBox()
