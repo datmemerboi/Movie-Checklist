b03348ec2fec0c5335e3869ddf32fbf292ac5427
datmemerboi <shravanssd@gmail.com>
2019-12-21 19:49:53 +0530

    Changed date format

--- backupnow.py
+++ backupnow.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def BackupNowFn(database, collection):
 	# now = datetime.now().strftime("_%d-%b-%y_%H:%M")
-	now = datetime.now().strftime("_%d-%m-%y_%H:%M")
+	now = datetime.now().strftime("_%d-%m-%Y_%H:%M")
 	filename = database+"_"+collection+now
 	query = "mongoexport --db "+database+" --collection "+collection+" --out "+os.path.realpath(os.getcwd())+"/backup/"+filename+".json"
 	os.system(query)
