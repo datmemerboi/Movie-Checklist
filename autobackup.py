79b7074cb692bd62670f4ed8b6bbc26b5fbebd22
datmemerboi <shravanssd@gmail.com>
2019-12-28 00:51:57 +0530

    Newest Auto Backup

---
+++ autobackup.py
@@ -0,0 +1,17 @@
+# Automatically importing most recent backup into Mongodb
+import os, re; from datetime import datetime
+thisyear = datetime.now().strftime("%y");thismonth = datetime.now().strftime("%m");thisday = datetime.now().strftime("%d")
+
+foundit = False
+
+def recentDateFn():
+	for name in os.listdir("backup"):
+		match = re.findall("Recommend"+"_"+thisday+"-"+thismonth+"-"+thisyear+"_[0-9]+:[0-9]+.json$", name)
+		if(match):
+			print(name+" is most recent")
+			foundit = True
+			break
+
+if(not(foundit)):
+	thisday = str(int(thisday)-1)
+	recentDateFn()
