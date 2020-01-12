0e6b8ff139a1cc8aa278587f19a6c07e23e488c6
datmemerboi <shravanssd@gmail.com>
2020-01-12 11:49:38 +0530

    Edited DateFormat

--- backupnow.py
+++ backupnow.py
@@ -2,8 +2,7 @@ import os
 from datetime import datetime
 
 def BackupNowFn(database, collection):
-	# now = datetime.now().strftime("_%d-%b-%y_%H:%M")
-	now = datetime.now().strftime("_%d-%m-%Y_%H:%M")
-	filename = database+"_"+collection+now
+	now = datetime.now().strftime("%d-%m-%Y")
+	filename = database+"_"+collection+"_"+now
 	query = "mongoexport --db "+database+" --collection "+collection+" --out "+os.path.realpath(os.getcwd())+"/backup/"+filename+".json"
 	os.system(query)
