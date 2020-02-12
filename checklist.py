9750c0ba8a4d98b9d9c2361ff703181e2b680706
datmemerboi <shravanssd@gmail.com>
2020-02-12 13:18:10 +0530

    Firebase design

--- checklist.py
+++ checklist.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtWidgets
-from pymongo import MongoClient
 import os, backupnow
+import mongo.insertion
 recommendBool = False
 
 class ChecklistClass(object):
@@ -67,64 +67,40 @@ class ChecklistClass(object):
 		self.remarks.resize(350, 150)
 		self.remarks.move(125, 315)
 
+		recommendBtn = QtWidgets.QCheckBox(window)
+		recommendBtn.setChecked(False)
+		recommendBtn.setStyleSheet(buttonCSS)
+		recommendBtn.resize(125, 30)
+		recommendBtn.move(165, 480)
+		recommendBtn.setText("Recommend?")
+		recommendBtn.stateChanged.connect(lambda:self.RecommendFn())
+
+		dbChoose = QtWidgets.QComboBox(window)
+		dbChoose.setStyleSheet(inputCSS)
+		dbChoose.addItems(("MongoDB", "Firebase"))
+		dbChoose.move(315, 480)
+		dbChoose.resize(120, 30)
+
 		addBtn = QtWidgets.QPushButton(window)
 		addBtn.setStyleSheet(buttonCSS)
 		addBtn.setText("Add")
 		addBtn.resize(70, 30)
-		addBtn.move(230, 480)
-		addBtn.clicked.connect(lambda: self.CheckExistingFn())
-		addBtn.clicked.connect(lambda: self.InsertCheckedFn())
+		addBtn.move(225, 535)
+		addBtn.clicked.connect(lambda: insertion.CheckExistingFn())
+		addBtn.clicked.connect(lambda: insertion.InsertCheckedFn())
 
-		recommendBtn = QtWidgets.QCheckBox(window)
-		recommendBtn.setChecked(False)
-		recommendBtn.setStyleSheet(buttonCSS)
-		recommendBtn.resize(15, 30)
-		recommendBtn.move(320, 480)
-		recommendBtn.stateChanged.connect(lambda:self.RecommendFn())
-		recommendText = QtWidgets.QLabel(window)
-		recommendText.setText("Recommend?")
-		recommendText.setStyleSheet(labelCSS)
-		recommendText.move(340, 478)
+		updateBtn = QtWidgets.QPushButton(window)
+		updateBtn.setStyleSheet(buttonCSS)
+		updateBtn.setText("Update")
+		updateBtn.resize(70, 30)
+		updateBtn.move(310, 535)
 
 		printListBtn = QtWidgets.QPushButton(window)
 		printListBtn.setStyleSheet(buttonCSS)
 		printListBtn.resize(180, 30)
-		printListBtn.move(215, 540)
+		printListBtn.move(215, 590)
 		printListBtn.setText("Print Checklist")
 		printListBtn.clicked.connect(lambda: os.system("python3 printlist.py"))
-
-	def CheckExistingFn(self):
-		mongoconn = MongoClient("mongodb://localhost:27017")
-		query = mongoconn.Movie.Checklist.find({"Title":self.title.text().strip()})
-		for i in query:
-			if(i['Title'] == self.title.text().strip() and i["Director"] == self.director.text().strip()):
-				ExistingMessage = QtWidgets.QMessageBox()
-				ExistingMessage.setText("Already in List")
-				ExistingMessage.setWindowTitle("Done!")
-				ExistingMessage.exec_()
-				return(True)
-		mongoconn.close()
-
-	def InsertCheckedFn(self):
-		if(not(self.CheckExistingFn())):
-			if(self.title.text().strip()=='' or self.director.text().strip()=='' or self.year.text().strip()==''):
-				NullMessage = QtWidgets.QMessageBox()
-				NullMessage.setText("Missing few values")
-				NullMessage.setWindowTitle("Error!")
-				NullMessage.exec_()
-			else:
-				row = {"Title":self.title.text().strip(), "Director":self.director.text().strip(), "Year":self.year.text().strip(), "Language":self.language.currentText()};
-				if(self.remarks.toPlainText().strip()!=''):
-					row["Remarks"] = self.remarks.toPlainText().strip()
-				mongoconn = MongoClient("mongodb://localhost:27017")
-				query = mongoconn.Movie.Checklist.update(row, row, upsert = True)
-				CheckedMessage = QtWidgets.QMessageBox()
-				CheckedMessage.setText("Added to checklist")
-				CheckedMessage.setWindowTitle("Done!")
-				CheckedMessage.exec_()
-				if(recommendBool):
-					query = mongoconn.Movie.Recommend.update(row, row, upsert=True)
-				mongoconn.close()
 
 	def RecommendFn(self):
 		global recommendBool
--- window.py
+++ window.py
@@ -1,5 +1,5 @@
 class WindowClass(object):
 	def WindowFn(self,window):
 		window.setWindowTitle("Window Class")
-		window.setFixedSize(600,600)
+		window.setFixedSize(600,650)
 		window.setStyleSheet("background-color:rgb(0, 0, 0);")
