18eb3f50a26b8f79ecba7345b06a0f43649ed718
datmemerboi <shravanssd@gmail.com>
2020-03-01 14:03:49 +0530

    Altered firebase

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 '''
-This is the file to setup your system for Movie Checklist.
+This is the file to setup your system for Movie Checklist on Debian-based system.
 Avoid it if you have the softwares pre-installed.
 
-Softwares Required: Python3, PyQt5, MongoDB, PyMongo
+Softwares Required: Python3, PyQt5, MongoDB, PyMongo & Firebase modules
 '''
 
 # Installing PyQt5
@@ -12,5 +12,11 @@ os.system("pip3 install --user pyqt5")
 os.system("sudo apt-get install python3-pyqt5")
 
 # Installing PyMongo
-print("Installing PyMongo..")
-os.system("python3 -m pip install pymongo")
+print("Installing PyMongo module..")
+os.system("sudo apt-get install python3-pip")
+os.system("python3 -m pip3 install pymongo")
+
+# Installing Firebase
+print("Installing Firebase module..")
+os.system("sudo apt-get install python3-pip")
+os.system("python3 -m pip3 install firebase")
