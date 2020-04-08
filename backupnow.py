24d9124937cb4afafdb99001a62fb20525433b7d
datmemerboi <shravanssd@gmail.com>
2020-04-08 13:31:21 +0530

    /backup for all backup functions

--- backupnow.py
+++
@@ -1,8 +0,0 @@
-import os
-from datetime import datetime
-
-def BackupNowFn(database, collection):
-	now = datetime.now().strftime("%d-%m-%Y")
-	filename = database+"_"+collection+"_"+now
-	query = "mongoexport --db "+database+" --collection "+collection+" --out "+os.path.realpath(os.getcwd())+"/backup/"+filename+".json"
-	os.system(query)
