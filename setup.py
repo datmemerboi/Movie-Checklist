8cc399c8abad8eb4553480219d5695594da53400
datmemerboi <shravanssd@gmail.com>
2020-04-08 13:30:43 +0530

    Travis test setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 This is the file to setup your system for Movie Checklist on Debian-based system.
 Avoid it if you have the softwares pre-installed.
 
-Softwares Required: Python3, PyQt5, MongoDB, PyMongo & Firebase modules
+Softwares Required: Python3 software, PyQt5, MongoDB, PyMongo & Firebase modules
 '''
 
 # Installing PyQt5
@@ -11,12 +11,7 @@ print("Installing PyQt5..")
 os.system("pip3 install --user pyqt5")
 os.system("sudo apt-get install python3-pyqt5")
 
-# Installing PyMongo
-print("Installing PyMongo module..")
+# Installing requirements.txt
+print("Installing necessary modules..")
 os.system("sudo apt-get install python3-pip")
-os.system("python3 -m pip3 install pymongo")
-
-# Installing Firebase
-print("Installing Firebase module..")
-os.system("sudo apt-get install python3-pip")
-os.system("python3 -m pip3 install firebase")
+os.system("python3 -m pip3 install -r requirements.txt")
